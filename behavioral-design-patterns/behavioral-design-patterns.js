65fdfe2000834050414f6694247bb2e2a21113d4
Alicio <alicio.alvesm@gmail.com>
2022-08-17 21:05:42 -0300

    feat: Behavioral Design Patterns

---
+++ behavioral-design-patterns/behavioral-design-patterns.js
@@ -0,0 +1,4 @@
+/*
+    In software engineering, behavioral design patterns are design patterns that identify common communication 
+    patterns among objects. By doing so, these patterns increase flexibility in carrying out communication.
+*/
